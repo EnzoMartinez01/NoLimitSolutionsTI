9d6f2786b4d48ee7256f21691a1d5b107e3d50b9
Thony-Leca <83093101+Thony-Leca@users.noreply.github.com>
2023-08-30 00:07:29 -0500

    nosotros_fix

--- src/Components/Nosotros/NosotrosMainComponent.jsx
+++ src/Components/Nosotros/NosotrosMainComponent.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import '../../Static/Css/nosotros.css';
+import avatar from '../../Static/Images/Nosotros/avatar.png';
 import bannernostros from '../../Static/Images/Nosotros/banner-nosotros.png';
 import imgmv from '../../Static/Images/Nosotros/nosotros-mv.png';
-import '../../Static/Css/nosotros.css';
 import valores1 from '../../Static/Images/Nosotros/valores1.png';
 import valores2 from '../../Static/Images/Nosotros/valores2.png';
 import valores3 from '../../Static/Images/Nosotros/valores3.png';
 import valores4 from '../../Static/Images/Nosotros/valores4.png';
 import anthony from '../../Static/Images/Principal/Anthony.png';
 import enzo from '../../Static/Images/Principal/Enzo.png';
-import avatar from '../../Static/Images/Nosotros/avatar.png';
 
 export default function NosotrosMainComponent() {
     return (
@@ -16,10 +16,9 @@ export default function NosotrosMainComponent() {
 
         <main>
             <section className='contenido'>
-                <section className='banner'>
+                <section className='bannern'>
                         <div className='banner-nostros-texto'>
                             <h1>¿Tiene interés en conocernos?</h1>
-                            <br />
                             <p>Nos complace compartir nuestros objetivos y la pasión que impulsó nuestra trayectoria en la industria de la consultoría de software</p>
                         </div>
                         <div className='banner-nosotros-img'>
@@ -47,20 +46,24 @@ export default function NosotrosMainComponent() {
 
                 <section>
                     <div className='mision-vision-container'>
+                        <div className='mision-vision-img'>
                         <img src={imgmv} alt="prueba" />
+                            </div>
                         <div className='mision-vision-div'>
-                            <div className='mision'></div>
+                            <div className='mision'>
                                 <h3>Misión</h3>
                                 <div>
                                     <p>Ser una consultoría líder en dar soluciones innovadoras orientadas a la era digital. Resolver los desafíos actuales de manera eficiente y efectiva, mientras capacitamos y empoderamos a futuros programadores a través de una formación espeScializada. Trabajamos en colaboración con nuestros clientes para construir un camino hacia el éxito tecnológico, impulsando el crecimiento y la evolución constante de empresas y profesionales en el ámbito tecnológico.</p>
                                 </div>
                             </div>
-                            <div className='vision'>
+                             <div className='vision'>
                                 <h3>Visión</h3>
                                 <div>
                                     <p> Aspiramos a transformar la industria a través de la innovación. Buscamos ser reconocidos por nuestro enfoque hacia el futuro y nuestra capacidad para resolver los desafíos emergentes. Así forjar un futuro en el que, las empresas y negocios, encuentren soluciones eficientes y sostenibles para sus necesidades tecnológicas, mientras inspiramos y capacitamos a una nueva generación de programadores que lideren el cambio en el mundo tecnológico. A través de nuestra dedicación y compromiso, nos esforzamos por crear un impacto positivo en la sociedad y contribuir al desarrollo tecnológico global.</p>
                                 </div>
+                            </div>   
                             </div>
+                            
                         </div>
                 </section>
 
@@ -107,7 +110,7 @@ export default function NosotrosMainComponent() {
                                         <h4>Anthony Lecarnaque</h4>
                                         <p>Development</p>
                                     </div>
-                                    <a href="https://www.linkedin.com/in/anthonylecarnaque/"><i class="fa-brands fa-linkedin"></i></a>
+                                    <a href="https://www.linkedin.com/in/anthonylecarnaque/" target='_blank'><i class="fa-brands fa-linkedin"></i></a>
                                 </div>
                             </div>
                             <div className='equipo-fotos'>
@@ -117,7 +120,7 @@ export default function NosotrosMainComponent() {
                                         <h4>Enzo Martinez</h4>
                                         <p>Development</p>
                                     </div>
-                                    <a href="https://www.linkedin.com/in/enzomartinez24/"><i class="fa-brands fa-linkedin"></i></a>
+                                    <a href="https://www.linkedin.com/in/enzomartinez24/" target='_blank'><i class="fa-brands fa-linkedin"></i></a>
                                 </div>
                             </div>
                             <div className='equipo-fotos'>
@@ -127,7 +130,8 @@ export default function NosotrosMainComponent() {
                                         <h4>Isabel Ortiz</h4>
                                         <p>Scrum Master</p>
                                     </div>
-                                    <i class="fa-brands fa-linkedin"></i>
+                                    <a href="" target='_blank'><i class="fa-brands fa-linkedin"></i>
+                                    </a>
                                 </div>
                             </div>
                         </div>
